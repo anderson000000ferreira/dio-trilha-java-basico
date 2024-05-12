f9903a83d004d1d308f7b393580a31afc29375ec
anderson000000ferreira <andersonciamicro@hotmail.com>
2024-05-12 14:40:21 -0300

    subindo o código do programa ContaTerminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        //TODO:Conhecer e Importar a classe Scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        //EXIBIR as mensgens par o noso usuario
+        System.out.println("Por favor, digite o número da Agência !");
+        String agencia = scanner.next();
+
+        System.out.println("por favor digite o seu código de úsuario !");
+        int numero = scanner.nextInt();
+
+        System.out.println("por favor digite seu Nome e Sobrenome ");
+        String nome = scanner.next();
+        String sobrenome = scanner.nextLine();
+        String nomeCliente = nome + " " + sobrenome;
+
+        System.out.println("Por favor digite seu saldo !");
+        double saldo = scanner.nextDouble();
+
+        //EXIBIR a mensagem conta criada
+        System.out.println("Olá " + nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo R$ " + saldo + " já está disponível para saque.");
     }
 }
